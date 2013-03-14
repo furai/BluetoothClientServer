4b7ead02e147424c8642d3278423734ae7c302b2
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2013-03-14 12:48:34 +0100

    [Added] consturctor for bluetooth client class.

--- src/dk/sdu/bluetoothclientserver/BTClientThreaded.java
+++ src/dk/sdu/bluetoothclientserver/BTClientThreaded.java
@@ -1,16 +1,21 @@
 package dk.sdu.bluetoothclientserver;
 
+import android.content.Context;
+
 public class BTClientThreaded {
 	
-	private class BtThread extends Thread
-	{
+	public BTClientThreaded(Context context, String mac) {
+
+	}
+
+	private class BtThread extends Thread {
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
 			super.run();
 		}
-		
+
 	}
 
 }

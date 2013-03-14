9f5c289fb2a8a25503100e92a9bf2449e406d3ad
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2013-03-14 12:46:01 +0100

    [added] New bluetooth client class.

---
+++ src/dk/sdu/bluetoothclientserver/BTClientThreaded.java
@@ -0,0 +1,16 @@
+package dk.sdu.bluetoothclientserver;
+
+public class BTClientThreaded {
+	
+	private class BtThread extends Thread
+	{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			super.run();
+		}
+		
+	}
+
+}

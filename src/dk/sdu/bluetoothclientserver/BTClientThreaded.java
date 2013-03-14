e278b55285f0cb88575e4a4ebc7b809f717d0778
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2013-03-14 12:54:02 +0100

    Implemented body of the constructor.

--- src/dk/sdu/bluetoothclientserver/BTClientThreaded.java
+++ src/dk/sdu/bluetoothclientserver/BTClientThreaded.java
@@ -1,10 +1,24 @@
 package dk.sdu.bluetoothclientserver;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 
 public class BTClientThreaded {
-	
+
+	private BluetoothAdapter adapter = null;
+	private BluetoothDevice device = null;
+	private BluetoothSocket btsocket = null;
+	private Context context = null;
+
+	private BtThread thread = null;
+
 	public BTClientThreaded(Context context, String mac) {
+
+		this.context = context;
+		this.adapter = BluetoothAdapter.getDefaultAdapter();
+		this.device = this.adapter.getRemoteDevice(mac);
 
 	}
 
